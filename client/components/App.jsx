a8e8e897067919b51ae8fce08b056515d7a9c4f3
Kimmi Rindel <kimmirindel97@gmail.com>
2018-03-01 14:01:38 +1300

    a necessary commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Route} from 'react-router-dom'
 
 import Home from './Home'
 
 const App = () => {
   return (
-    <Router>
-      <div className='app'>
-        <Route path='/' component={Home} />
-      </div>
-    </Router>
+    <div className='app'>
+      <Route path='/' component={Home} />
+    </div>
   )
 }
 
---
+++ client/index.js
@@ -0,0 +1,28 @@
+// https://dev.to/letsbsocial1/requestanimationframe--polyfill-in-react-16-2ce
+import 'core-js/es6/map'
+import 'core-js/es6/set'
+import React from 'react'
+import {render} from 'react-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunkMiddleware from 'redux-thunk'
+
+import reducers from './reducers'
+import App from './components/App'
+
+let store = createStore(reducers, compose(
+  applyMiddleware(thunkMiddleware),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+))
+
+document.addEventListener('DOMContentLoaded', () => {
+  render(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>,
+    document.getElementById('app')
+  )
+})
