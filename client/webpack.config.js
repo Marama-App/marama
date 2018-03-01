65089c7cf028a617c6e28b86fee4ba730e1b018d
Catherine Fromont <catherinegfromont@gmail.com>
2018-03-01 13:41:19 +1300

    fixed bundle error

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   entry: path.join(__dirname, '/index.js'),
   output: {
-    path: path.join(__dirname, '/'),
+    path: path.join(__dirname, '/../server/public'),
     filename: 'bundle.js'
   },
   module: {
