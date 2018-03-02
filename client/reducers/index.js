ac9d597d7fd749d0bacb7f4291de335ef1c22554
Elyse Wyatt <hi.elyse.wyatt@gmail.com>
2018-03-02 18:37:42 +1300

    Actions and reducers for grants.

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux'
 import interests from './interests'
 import interestType from './type'
 import typeDetails from './type-details'
+import grants from './grants'
 
 export default combineReducers({
   interests,
   interestType,
-  typeDetails
+  typeDetails,
+  grants
 })
