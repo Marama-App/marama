e86e82bac124de74fc9b4fe574cca80951eede0f
Kristina Muller <kristina.oneness@gmail.com>
2018-03-04 15:26:47 +1300

    Entered a couple of descriptions to seed files.

--- test/server/db/seeds/1_interests.js
+++ test/server/db/seeds/1_interests.js
@@ -4,10 +4,10 @@ exports.seed = (knex, Promise) => {
       return knex('interests').insert([
         {id: 1, interests: 'Gaming'},
         {id: 2, interests: 'Music'},
-        {id: 3, interests: 'Cars'},
-        {id: 4, interests: 'Skateboarding'},
-        {id: 5, interests: 'Dance'},
-        {id: 6, interests: 'Sports'}
+        {id: 3, interests: 'Sport'},
+        {id: 4, interests: 'Gadgets'},
+        {id: 5, interests: 'Dancing'},
+        {id: 6, interests: 'Food'}
       ])
     })
 }
