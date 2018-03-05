e2a614709489904985fde3e4f09ad63ef9696acf
Elyse Wyatt <hi.elyse.wyatt@gmail.com>
2018-03-06 11:10:46 +1300

    interest page kinda working

--- client/components/Interest.jsx
+++ client/components/Interest.jsx
@@ -10,25 +10,28 @@ class Interest extends React.Component {
 
   render () {
     return (
-      <div>
-      <img src='/images/bg-stars.png' className='stars-background'/>
-      <div className='page-section'>
-        <div className='interest-section'>
+      <div className='container'>
+        <img src='/images/bg-stars.png' className='stars-background' />
+        <div className='page-section'>
           <div className='page-title-font'>{this.props.match.params.interest}</div>
-          <div className='p-class'>If you love this, why not help create it?! Here's what you can do to become the builder of your favourite hobby.</div>
-          {this.props.interestType.map(type => (
-            <div key={type.type_id}>
-              <Link to={`/interests/${this.props.match.params.interest}/${type.name}`}>
+          <div className='p-class'>If you love this, why not help create it?! Here is what you can do to become the builder of your favourite hobby.</div>
+          <div className='interest-section'>
+            {this.props.interestType.map(type => (
+              <div key={type.type_id} className='thumbnails'>
                 <div className='h2-class'>{type.name}</div>
-              </Link>
-              <div>
-                <div className='p-class'>{type.description}</div>
+                <div>
+                  <div className='p-class'>{type.description}</div>
+                </div>
+                <Link to={`/interests/${this.props.match.params.interest}/${type.name}`}>
+                  <button className='interest-buttons'>
+                    Find out more
+                  </button>
+                </Link>
               </div>
-            </div>
-          )
-          )}
+            )
+            )}
+          </div>
         </div>
-      </div>
       </div>
     )
   }
