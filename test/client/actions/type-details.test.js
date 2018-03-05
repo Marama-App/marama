c1af30caf27bd8f6817b7a02a47761b98e20bc00
Emily Parkes <emilycoco@me.com>
2018-03-05 17:26:08 +1300

    receive type details action tests done and passing

--- test/client/actions/type-details.test.js
+++ test/client/actions/type-details.test.js
@@ -1,33 +1,33 @@
 /* global test expect */
 
-import { receiveType } from '../../../client/actions/type'
+import { receiveTypeDetail } from '../../../client/actions/type-details'
 
-test('receiveType returns the correct action type', () => {
-  const expected = 'RECEIVE_TYPE'
+test('receiveTypeDetail returns the correct action type', () => {
+  const expected = 'RECEIVE_TYPE_DETAIL'
 
-  const actual = receiveType()
+  const actual = receiveTypeDetail()
 
   expect(actual.type).toBe(expected)
 })
 
-test('receiveType returns the correct action', () => {
+test('receiveTypeDetail returns the correct action', () => {
   const expected = {
-    type: 'RECEIVE_TYPE'
+    type: 'RECEIVE_TYPE_DETAIL'
   }
 
-  const actual = receiveType()
+  const actual = receiveTypeDetail()
 
   expect(actual).toEqual(expected)
 })
 
-test('receiveType returns the interestType', () => {
-  const interestType = ['type1', 'type2']
+test('receiveTypeDetail returns the typeDetails', () => {
+  const typeDetails = ['typeDetail1', 'typeDetail2']
   const expected = {
-    type: 'RECEIVE_TYPE',
-    interestType: ['type1', 'type2']
+    type: 'RECEIVE_TYPE_DETAIL',
+    typeDetails: ['typeDetail1', 'typeDetail2']
   }
 
-  const actual = receiveType(interestType)
+  const actual = receiveTypeDetail(typeDetails)
 
   expect(actual).toEqual(expected)
 })
