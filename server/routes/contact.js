c2ce132863ebb8ecfe35ac20f46a268ddc05d9d2
Your Name <catherinegfromont@gmail.com>
2018-03-05 14:20:49 +1300

    contact component and form done, starting to get API call working

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const router = express.Router()
-// const development = require('../db/knexfile').development
-
-// const db = require('../db/db.js')
+// const apiKey = 'YOUR_API_KEY'
+// const DOMAIN = 'YOUR_DOMAIN_NAME'
+// const mailgun = require('mailgun-js')({apiKey: apiKey, domain: DOMAIN})
 
 module.exports = router
 
 router.use(bodyParser.json())
 
 router.post('/', (req, res) => {
-  console.log(req.body)
+  // console.log(req.body)
   res.send('okay')
 })
+
+// const data = {
+//   from: 'Excited User <me@samples.mailgun.org>',
+//   to: 'bar@example.com, YOU@YOUR_DOMAIN_NAME',
+//   subject: 'Hello',
+//   text: 'Testing some Mailgun awesomness!'
+// }
+
+// mailgun.messages().send(data, (error, body) => {
+// // console.log(body)
+// })
