de1def154083ec6ff58cd2a5ffd79b7d57e92d52
tian-dutoit <tiandutoit.nz@gmail.com>
2018-03-24 20:05:43 +1300

    NODE_ENV now updating after changing webpack to include plugin

--- client/actions/interests.js
+++ client/actions/interests.js
@@ -4,7 +4,6 @@ import {showError} from './error'
 import baseUrl from '../lib/base-url'
 
 export const RECEIVE_INTERESTS = 'RECEIVE_INTERESTS'
-let test = process.env.NODE_ENV
 
 export const receiveInterests = (interests) => {
   return {
@@ -15,7 +14,7 @@ export const receiveInterests = (interests) => {
 
 export function getInterests () {
   // eslint-disable-next-line no-console
-  console.log('base url', baseUrl, 'test', test, process.env.NODE_ENV)
+  console.log('base url', baseUrl, process.env.NODE_ENV)
   return (dispatch) => {
     request('get', `${baseUrl}/api/v1/interests`)
       .then(res => {
